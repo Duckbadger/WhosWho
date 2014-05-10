461f0525ee34f440b5713a840d9e4f8b91f0369b
Ken Boucher <Ken.boucher@3squared.com>
2014-05-10 16:24:24 +0100

    Created pod workspace Added HTMLParser object

---
+++ Who's Who/Who's Who/Helpers/HTMLParser.h
@@ -0,0 +1,13 @@
+//
+//  HTMLParser.h
+//  Who's Who
+//
+//  Created by Ken Boucher on 10/05/2014.
+//  Copyright (c) 2014 Ken Boucher. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HTMLParser : NSObject
+
+@end
