9d0ce2cf9a97a74ad13a4af16eb97200ecd32efd
Ken Boucher <Ken.boucher@3squared.com>
2014-05-10 17:29:43 +0100

    Added core data manager class

---
+++ Who's Who/Who's Who/Helpers/CoreDataManager.h
@@ -0,0 +1,13 @@
+//
+//  CoreDataManager.h
+//  Who's Who
+//
+//  Created by Ken Boucher on 10/05/2014.
+//  Copyright (c) 2014 Ken Boucher. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CoreDataManager : NSObject
+
+@end
---
+++ Who's Who/Who's Who/Helpers/CoreDataManager.m
@@ -0,0 +1,15 @@
+//
+//  CoreDataManager.m
+//  Who's Who
+//
+//  Created by Ken Boucher on 10/05/2014.
+//  Copyright (c) 2014 Ken Boucher. All rights reserved.
+//
+
+#import "CoreDataManager.h"
+
+@implementation CoreDataManager
+
+
+
+@end
