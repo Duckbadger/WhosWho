da865a126fe3d421b6e482988c6b262bd30b2566
Ken Boucher <Ken.boucher@3squared.com>
2014-05-10 17:08:57 +0100

    Initial parsing working now with hpple

--- Who's Who/Who's Who/Helpers/AppBusinessProfilesFetcher.m
+++ Who's Who/Who's Who/Helpers/AppBusinessProfilesFetcher.m
@@ -7,19 +7,39 @@
 //
 
 #import "AppBusinessProfilesFetcher.h"
+#import <TFHpple.h>
 
 @implementation AppBusinessProfilesFetcher
 
-+ (NSData *)dataForNSURL:(NSURL *)url
-{
-    NSData *htmlData = [NSData dataWithContentsOfURL:url];
-	
-	return htmlData;
-}
-
 + (NSArray *)fetchProfiles
 {
+	// First get the html data from the TAB profiles page
+	NSData *data = [NSData dataWithContentsOfURL:[NSURL URLWithString:@"http://www.theappbusiness.com/our-team/"]];
 	
+	// HTML parser
+	TFHpple *htmlParser = [TFHpple hppleWithHTMLData:data];
+	
+	/*
+	 *	Note - Format is as follows:
+	 *	Also note [] means there are multiples of this element
+	 *	<section id="users">	// Start here, class=wrapper is repeated elsewhere
+	 *		class="wrapper"
+	 *			[]<div class="row">
+	 *				[]<div class="col col2">
+	 *					<div class="title">
+	 *						<img />
+	 *					<h3> // Name
+	 *					<p> // Position
+	 *					<p class="user-description"> // Bio
+	 */
+	
+	//div[@class='column']
+    NSString *userSectionXpathQueryString = @"//div[@class='wrapper']";//@"<section id=\"users\">";
+	NSArray *userSectionArray = [htmlParser searchWithXPathQuery:userSectionXpathQueryString];
+	
+	
+
+	NSLog(@"end");
 	return @[];
 }
 
