bfbb37fbaabda1154bed9a3905d66f6ab0a63e97
Ken Boucher <Ken.boucher@3squared.com>
2014-05-21 19:27:31 +0100

    Added category for Photo

---
+++ Who's Who/Who's Who/Core Data/Photo+Extensions.m
@@ -0,0 +1,13 @@
+//
+//  Photo+Extensions.m
+//  Who's Who
+//
+//  Created by Ken Boucher on 21/05/2014.
+//  Copyright (c) 2014 Ken Boucher. All rights reserved.
+//
+
+#import "Photo+Extensions.h"
+
+@implementation Photo (Extensions)
+
+@end
